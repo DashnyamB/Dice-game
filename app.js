6a5ffedbbadc3e4e86e460fe578b7fbe227ecaf7
Dashnyam <ddashka166@gmail.com>
2020-02-07 21:04:15 +0800

    toglogchin eeljiig solidog bolgoh

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглэе
 
-var activePlayer = 1;
+var activePlayer = 0;
 
 // Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
 
@@ -24,9 +24,41 @@ document.getElementById("current-0").textContent = "0";
 
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
-
+// Шоог шидэх эвент листенэр
 document.querySelector(".btn-roll").addEventListener("click", function() {
+  // 1 - 6 хүртэл санатсаргүй тоог гаргана.
   var diceNumber = Math.floor(Math.random() * 6) + 1;
+  // Нуусан шоог гаргаж ирнэ.
   diceDom.style.display = "block";
+  // Буусан шооны тоонд харгалзах зургийг гаргаж ирнэ.
   diceDom.src = "dice-" + diceNumber + ".png";
+  //Буусан тоо 1 -с ялгаатай бол идэвхтэй тоглогчийн ээлжийн оноог нэмэгдүүлнэ.
+  if (diceNumber !== 1) {
+    //1 - с ялгаатай тоо буулаа. Буусан тоог тоглогчид нэмж өгнө.
+    roundScore = roundScore + diceNumber;
+    document.getElementById("current-" + activePlayer).textContent = roundScore;
+  } else {
+    // 1 буусан тул тоглогчийн ээлжийг энд солино.
+
+    //Энэ тоглогчийн оноог 0 болгоно.
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = 0;
+
+    //Тоглогчийн ээлжийг нөгөө тоглогчруу шилжүүлнэ.
+    // Хэрэв идэвхтэй тоглогч нь 0 байвал идэвхтэй тоглогчийг 1 болго.
+    // Үгүй бол идэвхтэй тоглогчийг 0 болго.
+    //Гурвалсан оператор ашиглав.
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+    //Улаан цэг шилжүүлэх
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    // if (activePlayer === 0) {
+    //   activePlayer = 1;
+    // } else {
+    //   activePlayer = 0;
+    // }
+
+    //Шоог түр алга болгох
+    diceDom.style.display = "none";
+  }
 });
